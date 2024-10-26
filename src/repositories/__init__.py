e602cd95dfebe8f9979bf3dd5773935165b3656a
Rafael Ferreira <rafaelcferreira@ua.pt>
2024-10-26 04:50:07 +0100

    Database manager class

--- src/repositories/__init__.py
+++ src/repositories/__init__.py
@@ -1,41 +1,44 @@
-# __all__ = ["connection"]
+__all__ = ["connection", "session"]
 
-# from dotenv import load_dotenv
-# from os import getenv
-# from sqlalchemy import create_engine
-# from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.ext.declarative import declarative_base
-
-
-# class __Database:
-#     __engine = None
-#     __SessionLocal = None
-#     Base = None
-
-#     def __init__(self):
-#         self.__save_connection_string(*self.__load_database_info())
-#         self.__engine = create_engine(self.conn_string)
-#         self.__SessionLocal = sessionmaker(autoflush=False, bind=self.__engine)
-#         self.Base = declarative_base()
-
-#     def __save_connection_string(self, user, password, host, port, database):
-#         self.conn_string = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-
-#     def __load_database_info(self):
-#         load_dotenv(".env")
-#         fields = ["USER", "PASSWORD", "HOST", "PORT", "DATABASE"]
-#         return tuple(getenv(term) for term in fields)
-
-#     def session(self):
-#         db = self.__SessionLocal()
-#         try:
-#             yield db
-#         finally:
-#             db.close()
-
-#     def conn(self):
-#         return self.__engine.connect()
+from dotenv import load_dotenv
+from os import getenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy_utils import database_exists, create_database
+from ..models.utils import Base
 
 
-# database = __Database()
-# connection = database.conn()
+class __Database:
+    __engine = None
+    __SessionLocal = None
+    Base = None
+
+    def __init__(self):
+        self.__save_connection_string(*self.__load_database_info())
+        if not database_exists(self.conn_string):
+            create_database(self.conn_string)
+        self.__engine = create_engine(self.conn_string, echo=False)
+        self.init_database()
+        self.__SessionLocal = sessionmaker(autoflush=False, bind=self.__engine)
+
+    def __save_connection_string(self, user, password, host, port, database):
+        self.conn_string = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+    def __load_database_info(self):
+        load_dotenv(".env")
+        fields = ["USER", "PASSWORD", "HOST", "PORT", "DATABASE"]
+        return tuple(getenv(term) for term in fields)
+
+    def init_database(self):
+        Base.metadata.create_all(bind=self.__engine)
+
+    @property
+    def session(self):
+        return self.__SessionLocal()
+
+    @property
+    def conn(self):
+        return self.__engine.connect()
+
+
+database = __Database()
