69b773b9a0a89631b59d31360b97dab6aa060c56
Rafael Ferreira <rafaelcferreira@ua.pt>
2024-10-26 04:49:08 +0100

    OO instead of functional

--- src/models/client.py
+++ src/models/client.py
@@ -1,12 +1,19 @@
-from sqlalchemy import Table, Column, LargeBinary
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from .utils import *
 
-client_table = Table(
-    "client",
-    metadata,
-    id_,
-    name,
-    phone,
-    email,
-    Column("picture", LargeBinary),
-)
+class Client(Base):
+    
+    __tablename__ = "client"
+    
+    id = id_
+    name = name 
+    phone = phone
+    email = email
+    picture = Column(String)
+    
+    # client = relationship("Client", back_populates="client")
+        
+    def __repr__(self):
+        return f"<Client(id={self.id}, name={self.name}, phone={self.phone}, email={self.email}, picture={self.picture})>"
+    
--- src/models/utils.py
+++ src/models/utils.py
@@ -5,6 +5,7 @@ from sqlalchemy import (
     String,
     MetaData,
 )
+from sqlalchemy.orm import declarative_base
 
 metadata = MetaData()
 
@@ -12,3 +13,5 @@ id_ = Column("id", Integer, Identity(start=0, increment=1), primary_key=True)
 name = Column("name", String)
 phone = Column("phone", String)
 email = Column("mail", String)
+
+Base = declarative_base(metadata=metadata)
